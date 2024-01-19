57f84cc37b246fb16ccd7155fc5cd6c5697adb96
phamquocttrung <thuyminmin920@gmail.com>
2024-01-19 11:43:26 +0700

    Design UI for  Project

---
+++ app/src/main/java/com/example/abrrapp/activities/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.abrrapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.abrrapp.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
---
+++ app/src/main/java/com/example/abrrapp/activities/HistoryOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.abrrapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.abrrapp.R;
+
+public class HistoryOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_order);
+    }
+}
